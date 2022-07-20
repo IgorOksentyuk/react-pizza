1f7946b8deb1a8fd5b9964385d531fe192cd2e4c
IgorOks <winbka.lem@gmail.com>
2022-07-20 15:23:19 +0300

    Створив папку зі сторінками для навігації по сайту

---
+++ src/pages/Home.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import Categories from '../Components/Categories';
+import Sort from '../Components/Sort';
+import PizzaBlock from '../Components/PizzaBlock';
+import Skeleton from '../Components/PizzaBlock/Skeleton';
+
+const Home = () => {
+  const [pizzas, setPizzas] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(true);
+
+  React.useEffect(() => {
+    fetch('https://62987872f2decf5bb7434cfd.mockapi.io/pizza')
+      .then((res) => res.json())
+      .then((arr) => {
+        setPizzas(arr);
+        setIsLoading(false);
+      });
+  }, []);
+  return (
+    <div className="container">
+      <div className="content__top">
+        <Categories />
+        <Sort />
+      </div>
+      <h2 className="content__title">Всі піцци</h2>
+      <div className="content__items">
+        {isLoading
+          ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
+          : pizzas.map((item) => <PizzaBlock key={item.id} {...item} />)}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
