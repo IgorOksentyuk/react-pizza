0ff9caffc534b8cfb35b77296e455a2f5ce4bd36
IgorOks <winbka.lem@gmail.com>
2022-07-25 17:14:28 +0300

    переніс стейт до батьківського компонента Home і через пропси закинув стейт і сетстейт у компонент категорій

--- src/Components/Categories.jsx
+++ src/Components/Categories.jsx
@@ -1,23 +1,14 @@
 import React from 'react';
 
-const Categories = () => {
-  const [activeIndex, setActiveIndex] = React.useState(0);
-
+const Categories = ({ value, onClickCategory }) => {
   const categories = ['Всі', 'М`ясні', 'Вегетаріанські', 'Гриль', 'Гострі', 'Закриті'];
-
-  const onClickCategory = (index) => {
-    setActiveIndex(index);
-  };
 
   return (
     <div className="categories">
       <ul>
-        {categories.map((value, i) => (
-          <li
-            key={i}
-            onClick={() => onClickCategory(i)}
-            className={activeIndex === i ? 'active' : ''}>
-            {value}
+        {categories.map((categoryName, i) => (
+          <li key={i} onClick={() => onClickCategory(i)} className={value === i ? 'active' : ''}>
+            {categoryName}
           </li>
         ))}
       </ul>
