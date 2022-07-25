6c72b1bc50eda3f925abaedfe317cb505039d354
IgorOks <winbka.lem@gmail.com>
2022-07-25 17:19:53 +0300

    Додав дочірні стейти від Categories i Sort, за допомоою mockapi через фетч здійснив фільтрацію за категоріями та сортування

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,21 +7,35 @@ import Skeleton from '../Components/PizzaBlock/Skeleton';
 const Home = () => {
   const [pizzas, setPizzas] = React.useState([]);
   const [isLoading, setIsLoading] = React.useState(true);
+  const [categoryId, setCategoryId] = React.useState(0);
+  const [sortType, setSortType] = React.useState({
+    name: 'популярністю',
+    sortProperty: 'rating',
+  });
 
   React.useEffect(() => {
-    fetch('https://62987872f2decf5bb7434cfd.mockapi.io/pizza')
+    setIsLoading(true);
+    const category = categoryId > 0 ? `category=${categoryId}` : '';
+    const sortBy = sortType.sortProperty.replace('-', '');
+    const order = sortType.sortProperty.includes('-') ? 'asc' : 'desc';
+
+    fetch(
+      `https://62987872f2decf5bb7434cfd.mockapi.io/pizza?${category}&sortBy=${sortBy}&order=${order}`,
+    )
       .then((res) => res.json())
       .then((arr) => {
         setPizzas(arr);
         setIsLoading(false);
       });
+
     window.scrollTo(0, 0);
-  }, []);
+  }, [categoryId, sortType]);
+
   return (
     <div className="container">
       <div className="content__top">
-        <Categories />
-        <Sort />
+        <Categories value={categoryId} onClickCategory={(id) => setCategoryId(id)} />
+        <Sort value={sortType} onClickSort={(id) => setSortType(id)} />
       </div>
       <h2 className="content__title">Всі піцци</h2>
       <div className="content__items">
