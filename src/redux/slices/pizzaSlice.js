586643aa8936fe0e3256a77cbdfebc3dc14fd3ad
IgorOks <winbka.lem@gmail.com>
2022-08-10 16:30:34 +0300

    Створив слайс, за допомогою createAsyncThunk зробив запит на бек та за допомогою extraReducers зробив завантаження та помилку

--- src/redux/slices/pizzaSlice.js
+++ src/redux/slices/pizzaSlice.js
@@ -1,7 +1,17 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+export const fetchPizzas = createAsyncThunk('pizza/fetchPizzasStatus', async (params) => {
+  const { category, sortBy, order, search, currentPage } = params;
+  const { data } = await axios.get(
+    `https://62987872f2decf5bb7434cfd.mockapi.io/pizza?page=${currentPage}&limit=4&${category}&sortBy=${sortBy}&order=${order}${search}`,
+  );
+  return data;
+});
 
 const initialState = {
   items: [],
+  status: 'loading',
 };
 
 const pizzaSlice = createSlice({
@@ -10,6 +20,20 @@ const pizzaSlice = createSlice({
   reducers: {
     setItems(state, action) {
       state.items = action.payload;
+    },
+  },
+  extraReducers: {
+    [fetchPizzas.pending]: (state) => {
+      state.status = 'loading';
+      state.items = [];
+    },
+    [fetchPizzas.fulfilled]: (state, action) => {
+      state.items = action.payload;
+      state.status = 'success';
+    },
+    [fetchPizzas.rejected]: (state) => {
+      state.status = 'error';
+      state.items = [];
     },
   },
 });
