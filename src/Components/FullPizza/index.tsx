3822781aeaffad2a158f006068b16d71c62b9a3c
IgorOks <winbka.lem@gmail.com>
2022-08-23 14:30:07 +0300

    Типізував компонент, useState та змінив формат файлу на tsx

---
+++ src/Components/FullPizza/index.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import '../FullPizza/FullPizza.scss';
+
+const FullPizza: React.FC = () => {
+  const [pizza, setPizza] = React.useState<{
+    imageUrl: string;
+    title: string;
+    price: string;
+  }>();
+  const { id } = useParams();
+  const navigate = useNavigate();
+
+  React.useEffect(() => {
+    async function fetchPizza() {
+      try {
+        const { data } = await axios.get('https://62987872f2decf5bb7434cfd.mockapi.io/pizza/' + id);
+        setPizza(data);
+      } catch (error) {
+        alert('Помилка при отриманні піцци!');
+        navigate('/');
+      }
+    }
+
+    fetchPizza();
+  }, []);
+
+  if (!pizza) {
+    return <>Завантаження...</>;
+  }
+  return (
+    <div className="root">
+      <img src={pizza.imageUrl} />
+      <h2>{pizza.title}</h2>
+      <h4>{pizza.price} ₴</h4>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat, porro?</p>
+      <button className="button button--outline button--add">
+        <svg
+          width="12"
+          height="12"
+          viewBox="0 0 12 12"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg">
+          <path
+            d="M10.8 4.8H7.2V1.2C7.2 0.5373 6.6627 0 6 0C5.3373 0 4.8 0.5373 4.8 1.2V4.8H1.2C0.5373 4.8 0 5.3373 0 6C0 6.6627 0.5373 7.2 1.2 7.2H4.8V10.8C4.8 11.4627 5.3373 12 6 12C6.6627 12 7.2 11.4627 7.2 10.8V7.2H10.8C11.4627 7.2 12 6.6627 12 6C12 5.3373 11.4627 4.8 10.8 4.8Z"
+            fill="white"
+          />
+        </svg>
+        <span>Додати</span>
+      </button>
+    </div>
+  );
+};
+
+export default FullPizza;
