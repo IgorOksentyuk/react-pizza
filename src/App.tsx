283c5a153be1f655656820889b0cc10879e016fa
IgorOks <winbka.lem@gmail.com>
2022-08-23 14:28:45 +0300

    змінив формат файлу на tsx

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+import Header from './Components/Header';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
+import Cart from './pages/Cart';
+import FullPizza from './Components/FullPizza';
+
+import './scss/app.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="wrapper">
+        <Header />
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/pizza/:id" element={<FullPizza />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
