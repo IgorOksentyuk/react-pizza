136351ded007c0ae16e29ded4ba65ae7f6a07b05
IgorOks <winbka.lem@gmail.com>
2022-08-30 14:54:15 +0300

    Прибрав return

--- src/Components/CartEmpty.tsx
+++ src/Components/CartEmpty.tsx
@@ -2,23 +2,21 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import emptyCartImg from '../assets/img/empty-cart.png';
-const CartEmpty: React.FC = () => {
-  return (
-    <div className="cart cart--empty">
-      <h2>
-        Корзина пуста <span>😕</span>
-      </h2>
-      <p>
-        Ви ще не замовили піцу.
-        <br />
-        Для того, щоб замовити піцу, перейдіть на головну сторінку.
-      </p>
-      <img src={emptyCartImg} alt="Empty cart" />
-      <Link to="/" className="button button--black">
-        <span>Повернутися назад</span>
-      </Link>
-    </div>
-  );
-};
+const CartEmpty: React.FC = () => (
+  <div className="cart cart--empty">
+    <h2>
+      Корзина пуста <span>😕</span>
+    </h2>
+    <p>
+      Ви ще не замовили піцу.
+      <br />
+      Для того, щоб замовити піцу, перейдіть на головну сторінку.
+    </p>
+    <img src={emptyCartImg} alt="Empty cart" />
+    <Link to="/" className="button button--black">
+      <span>Повернутися назад</span>
+    </Link>
+  </div>
+);
 
 export default CartEmpty;
